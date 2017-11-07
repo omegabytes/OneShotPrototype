06d059f5b168cad6440ce1510cdb2aa6ea2aa58f
Alex Gaesser <agaesser@gmail.com>
2017-11-07 12:02:34 -0800

    testing json structure

--- src/dndLib.js
+++ src/dndLib.js
@@ -14,28 +14,6 @@ exports.notFoundMessage = function(slotName, userInput) {
     return speechOutput;
 };
 
-// returns the page(s) where a subject can be found
-// in the 5e player's handbook
-// exports.pageFind = function(index, indexName) {
-//     var pageString = "";
-//     if(typeof index.pages === 'string'){
-//         pageString = index.pages
-//     }
-//     else{
-//         if(index.pages.length>1){
-//             pageString += "pages ";
-//             for(var i = 0; i <= index.pages.length-2; i++){
-//                 pageString += index.pages[i] + ", "
-//             }
-//             pageString += "and " + index.pages[index.pages.length-1]
-//         }
-//         else{
-//             pageString = "page " + index.pages
-//         }
-//     }
-//     return indexName + " can be found on " + pageString + ". ";
-// };
-
 // roll dice function
 exports.rollDice = function(quantity,sides) {
     var facevalue;
